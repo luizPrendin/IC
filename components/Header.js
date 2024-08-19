88b72d5dad31218e7cd9334db6ca6e0621939ff2
luizPrendin <luizprendinfelipe@gmail.com>
2024-08-19 19:41:29 -0300

    Cabecalho parcial

--- components/Header.js
+++ components/Header.js
@@ -1,51 +1,87 @@
 import React from 'react';
 import { View, Image, Text, StyleSheet, SafeAreaView } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function Header() {
   return (
     <SafeAreaView style={styles.headerContainer}>
-      <View style={styles.container}>
-        <Image source={require('../assets/logo.jpeg')} style={styles.logo} />
+      <LinearGradient
+        colors={['#6030A0', '#FFAFFF']}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        style={styles.header}
+      >
+        <Image
+          source={{ uri: "../assets/FotoHome.png" }} // Replace with the actual image URL
+          style={styles.profileImage}
+        />
+        
         <View style={styles.textContainer}>
-          <Text style={styles.text}>CP02HardMode</Text>
-          <Text style={styles.subText}>Professor Fernando Pinéo</Text>
+          <Text style={styles.greeting}>Bom dia</Text>
+          <Text style={styles.name}>Marília</Text>
         </View>
-      </View>
+        <View style={styles.headerItem}>
+          <Image
+          source={{ uri: "../Key Visual/yaso1.png" }} // Replace with the actual image URL
+          style={styles.logo}
+        />
+        <Image
+          source={{ uri: "../Key Visual/config.png" }} // Replace with the actual image URL
+          style={styles.settingsIcon}
+        />
+        </View>
+        
+        
+      </LinearGradient>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   headerContainer: {
-    backgroundColor: '#ff006c',
-    height: '10%',
-    marginTop: 50,
-    alignItems: 'flex-start',
-    paddingLeft: 10,
-    justifyContent: 'center',
+    flex:1,
+    backgroundColor: '#f0e7ff',
+    
+    paddingBottom:25
   },
-  container: {
+  header: {
     flexDirection: 'row',
     alignItems: 'center',
-    padding: 10,
+    padding: 20,
   },
-  logo: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    marginRight: 10,
-    marginLeft: 5,
+  headerItem:{
+    flexDirection:"row",
+    paddingLeft:20,
+    marginStart:1170,
+    justifyContent:"space-between",
+  },
+  profileImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
   },
   textContainer: {
     marginLeft: 10,
+    flexDirection: 'column',
   },
-  text: {
+  greeting: {
+    color: '#fff',
+    fontSize: 14,
+  },
+  name: {
+    color: '#fff',
     fontSize: 18,
     fontWeight: 'bold',
-    color: '#fff',
   },
-  subText: {
-    fontSize: 14,
-    color: '#fff',
+  logo: {
+    width: 90,
+    height: 50,
+  },
+  settingsIcon: {
+    width: 30,
+    height: 30,
+    marginLeft:35,
+    marginTop:20,
+    paddingTop:30,
   },
 });
